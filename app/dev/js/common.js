f1a1b9ce3eddf44c496b53f5ae74d37a7cee59e3
sweelemk <sweelemk@gmail.com>
2015-12-06 19:52:20 +0300

    test version

--- app/dev/js/common.js
+++ app/dev/js/common.js
@@ -7,7 +7,19 @@ $(document).ready(function () {
 			padding: 0,
 			type:'inline',
 			scrolling: 'visible',
-			margin: [100, 50, 50, 100],
+			autoHeight: true,
+			margin: [50, 10, 10, 10],
+			tpl: {
+				closeBtn: '<a title="Close" class="fancybox-item fancybox-close" href="javascript:;"><svg viewBox="0 0 100.4 101" xmlns="http://www.w3.org/2000/svg"><g><path id="svg_3" d="m70.949997,29.77034c-0.781006,-0.781998 -2.047005,-0.781998 -2.828003,0l-17.954998,17.957001l-17.954998,-17.957001c-0.780998,-0.781998 -2.047001,-0.781998 -2.827999,0c-0.781002,0.781002 -0.781002,2.047001 0,2.828003l17.954998,17.956997l-17.954998,17.957005c-0.781002,0.779999 -0.781002,2.047005 0,2.828003c0.390999,0.389999 0.902,0.584999 1.413998,0.584999s1.023003,-0.195 1.414001,-0.584999l17.954998,-17.956005l17.954998,17.956005c0.390999,0.389999 0.902,0.584999 1.414001,0.584999s1.023003,-0.195 1.414001,-0.584999c0.780998,-0.780998 0.780998,-2.048004 0,-2.828003l-17.954998,-17.957005l17.954998,-17.956997c0.781998,-0.780003 0.781998,-2.047001 0,-2.828003zm-20.783001,-29.344997c-27.641998,0 -50.128998,22.487 -50.128998,50.126999c0.002,27.643002 22.491003,50.131001 50.133003,50.131001c27.638996,0 50.124996,-22.488998 50.124996,-50.131001c0,-27.639999 -22.487,-50.126999 -50.129002,-50.126999zm0.004005,96.259007l-0.002007,0c-25.434996,0 -46.128995,-20.695007 -46.130997,-46.131008c0,-25.434998 20.693001,-46.126998 46.128998,-46.126998s46.129009,20.693002 46.129009,46.126998c-0.000008,25.435001 -20.69101,46.131008 -46.125004,46.131008z"/></g></svg></a>',
+				next: '<a title="Next" class="fancybox-nav fancybox-next" href="javascript:;"><span><svg viewBox="0 0 512 512" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M129.6,512c-4.1,0-8.1-1.6-11.1-4.8c-5.7-6.1-5.4-15.7,0.8-21.5l241.2-224.5L119,26.1  c-6-5.9-6.1-15.5-0.3-21.5c5.9-6,15.5-6.1,21.5-0.3L393,250.5c3,2.9,4.7,6.9,4.6,11.1c-0.1,4.2-1.8,8.1-4.8,10.9L139.9,507.9  C137,510.7,133.3,512,129.6,512z"/></svg></span></a>',
+				prev: '<a title="Previous" class="fancybox-nav fancybox-prev" href="javascript:;"><span><svg viewBox="0 0 512 512" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M382.5,512c-3.7,0-7.4-1.3-10.3-4.1L119.2,272.5c-3-2.8-4.8-6.8-4.8-10.9c-0.1-4.2,1.6-8.2,4.6-11.1  L371.9,4.3c6-5.8,15.6-5.7,21.5,0.3c5.8,6,5.7,15.6-0.3,21.5L151.6,261.2l241.2,224.5c6.1,5.7,6.5,15.3,0.8,21.5  C390.6,510.4,386.5,512,382.5,512z"/></svg></span></a>'
+			},
+			afterLoad: function() {
+				//$('html').addClass('space')
+			},
+			afterClose: function() {
+				//$('html').removeClass('space')
+			}
 			//arrows: false
 			// helpers: {
 			// 	title: {
@@ -19,6 +31,7 @@ $(document).ready(function () {
 			// }
 		});
 	})();
+	
 
 	//tab
 	(function(){
@@ -92,7 +105,7 @@ $(document).ready(function () {
 	//POPUP INIT
 
 	(function(){
-		var duration = 500,
+		var duration = 250,
 			popupSelector = $('.popup__wrap'),
 			innerSelector = $('.popup'),
 			html = $('html');
@@ -100,7 +113,8 @@ $(document).ready(function () {
 		$('.js-popup-link').on('click', function(event){
 			if(!$(this).hasClass('.js-popup-data')){
 				tabs();
-				//return false;
+
+				$('.popup').find('.tab__menu-container').find('.tab-item').first().find('label:first-child input').prop('checked', true);
 			}
 			var popup = $(this).data('href');
 
@@ -119,7 +133,7 @@ $(document).ready(function () {
 			event.stopPropagation();
 		});
 
-		$(".popup__close, .popup__wrap").on("click", function(){	
+		$(".popup__close, .sucess__close, .popup__wrap").on("click", function(){	
 			if(!popupSelector.hasClass('is-visible')) return;
 			
 			popupSelector
@@ -218,7 +232,7 @@ $(document).ready(function () {
 			if ($(this).length > 0) {
 				$(this).validate({
 					submitHandler: function(form) {
-						//ajaxSubmit(form);
+						ajaxSubmit(form);
 					},
 					errorClass: 'has-error',
 					rules: {
@@ -326,10 +340,10 @@ $(document).ready(function () {
 		console.log(tab,check)
 
 		if (form.valid()) {
-			steps.removeClass("is-visible").hide();
-			step.addClass('is-visible').show();
+			steps.removeClass("is-visible").fadeOut();
+			step.fadeIn(200).addClass('is-visible');
 			step.find('.tab__list').find('a[data-person="'+tab+'"]').parent().addClass('is-active').siblings().removeClass('is-active').addClass('no-active');
-			step.find('.tab__menu-container').find('input[data-days="'+check+'"]').parents('.tab-item').show().addClass('visible').siblings().removeClass('visible');
+			step.find('.tab__menu-container').find('input[data-days="'+check+'"]').parents('.tab-item').show().addClass('visible').siblings().removeClass('visible').hide();
 			step.find('.tab__menu-container').find('input[data-days="'+check+'"]').prop("checked",true).parents('.tab__menu-container').find('input:not(:checked)').prop("disabled", true);
 			contPrice.text(price + 'бел.руб.')
 			return false;
@@ -338,4 +352,44 @@ $(document).ready(function () {
 			return false;
 		}
 	});
+
+	$(".js-prev-step").on("click", function(){
+		if ($(this).parent().hasClass("is-active") && !$(this).parent().hasClass("is-current")) {
+			var steps = $('.popup__wrap');
+			var step = $('.popup__wrap[data-step="'+$(this).attr("data-step")+'"]');
+			steps.removeClass("is-visible").fadeOut();
+			step.fadeIn(200).addClass("is-visible");
+			return false;
+		}
+		return false;
+	});
+
+	(function(){
+		$('a[data-scroll]').click(function(){
+			var idscroll = $(this).data('scroll');
+			$.scrollTo(idscroll, 1000);
+			return false;
+		});
+	})();
+
+	(function(){
+		var feed = new Instafeed({
+			clientId: '97ae5f4c024c4a91804f959f43f2635f',
+			target: 'instafeed',
+			get: 'tagged',
+			tagName: 'photographyportfolio',
+			links: true,
+			limit: 16,
+			sortBy: 'random',
+			resolution: 'thumbnail',
+			template: '<div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a></div></div>'
+		});
+		feed.run();
+
+		$('.btn__sync').on('click', function () {
+			$('#instafeed').empty();
+			feed.run();
+		})
+
+	})();
 });
